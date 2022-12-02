912b6728a995a9d3df830f03989605bb0a63be61
digital <digital@iMac24-8.local>
2022-12-02 10:41:08 +0100

    init

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by digital on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
